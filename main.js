a4e3b25220754677a43ce7235a7ae913fb6a9d03
bachidze <bachidze90@gmail.com>
2023-11-04 23:28:32 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const brgr = document.querySelector('.brgr')
+const brgrDiv = document.querySelector('.brgr-div')
+
+brgr.addEventListener('click', () => {
+    brgrDiv.classList.toggle('hidden')
+})
